4df60523fe1417f5bd06fcb1f47397e75d7ad22b
diegoavila14 <diegoavila14@gmail.com>
2014-11-17 01:29:12 -0300

    Guardar y al tuto

--- ProyectoGrupo15/src/UseCase/ModoGraficoManager.java
+++ ProyectoGrupo15/src/UseCase/ModoGraficoManager.java
@@ -15,7 +15,9 @@ import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.swing.JScrollBar;
@@ -23,9 +25,14 @@ import javax.swing.ScrollPaneConstants;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
+import UseCaseEditor.Contenedor;
+import UseCaseEditor.Union;
+
 import java.awt.GridLayout;
 import java.io.File;
 import java.io.IOException;
+import javax.swing.DebugGraphics;
+import javax.swing.JScrollPane;
 
 public class ModoGraficoManager extends JFrame {
 
@@ -33,6 +40,11 @@ public class ModoGraficoManager extends JFrame {
 	final JButton btnPng;
 	final JButton btnVolver;
 	Diagram d;
+	List<Union> Uniones;
+	List<Integer> CantConexiones;
+	int lineArrow;
+	Contenedor con;
+	List<Entity> entidades;
 	
 	Map<String, Entity> map; //diccionario para linkear id con entidad
 
@@ -73,8 +85,22 @@ public class ModoGraficoManager extends JFrame {
 				
 		
 		this.d = D;
+		
+		entidades = new ArrayList<Entity>();
+		Uniones = new ArrayList<Union>();
+		lineArrow = 10;
+		CantConexiones = new ArrayList<Integer>();
+		
 		setUserCases();
 		setActors();
+		
+		con = new Contenedor(Uniones);
+		
+		for(int i = 0 ; i< entidades.size(); i++){
+			con.add(entidades.get(i));
+		}
+		//setViewportView(con);
+		con.setLayout(null);
 	}
 	
 	public void getPNG(String nFile)
@@ -112,6 +138,7 @@ public class ModoGraficoManager extends JFrame {
 			entity.setBounds(450,auxY,aprox,70);
 			auxY += 90;
 			map.put(uc.id, entity);
+			entidades.add(entity);
 			getContentPane().add(entity);
 		}
 	}
@@ -140,6 +167,7 @@ public class ModoGraficoManager extends JFrame {
 				auxS += 90;
 			}
 			map.put(a.id, entity);
+			entidades.add(entity);
 			getContentPane().add(entity);
 		}
 		
